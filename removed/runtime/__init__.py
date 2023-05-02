e8cc7bfeb5a39031062dbab06219bee8d9afbece
thirdgerb <thirdgerb@gmail.com>
2023-05-03 02:30:56 +0800

    dev: remove removed files

--- removed/runtime/__init__.py
+++
@@ -1,8 +0,0 @@
-from ghoshell.ghost_fmk.runtime.driver import AbsRuntimeDriver, CacheRuntimeDriver
-from ghoshell.ghost_fmk.runtime.runtime import IRuntime
-
-__all__ = [
-    "IRuntime",
-    "AbsRuntimeDriver",
-    "CacheRuntimeDriver",
-]
