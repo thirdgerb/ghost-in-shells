631ac88c2f1a53f930bec063cc6a8bc4221d6dfa
thirdgerb <thirdgerb@gmail.com>
2023-05-15 18:11:51 +0800

    test: test about context relative faq

--- ghoshell/llms/providers.py
+++ ghoshell/llms/providers.py
@@ -17,7 +17,7 @@ class LangChainOpenAIPromptProvider(Provider):
 
     def factory(self, con: Container, params: Dict | None = None) -> Contract | None:
         ai = OpenAI(
-            request_timeout=10,
+            request_timeout=15,
             max_tokens=2048,
             model_name="text-davinci-003",
             max_retries=0,
