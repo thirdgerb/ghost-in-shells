05ab9b1ead15a5b3cb056c9f49daa9b24bf27b90
thirdgerb <thirdgerb@gmail.com>
2023-07-20 18:15:31 +0800

    refact: use poetry scripts replaceing demo.py

--- ghoshell/mocks/ghost_mock/ghost_mock.py
+++ ghoshell/mocks/ghost_mock/ghost_mock.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, ClassVar
 
 from ghoshell.container import Provider
 from ghoshell.framework.bootstrapper import FileLoggerBootstrapper, \
@@ -12,49 +12,51 @@ from ghoshell.mocks.providers import *
 from ghoshell.prototypes.playground.llm_test_ghost import GameUndercoverBootstrapper
 from ghoshell.prototypes.playground.llm_test_ghost import LLMConversationalThinkBootstrapper, \
     PromptUnitTestsBootstrapper
-from ghoshell.prototypes.playground.sphero import SpheroGhostBootstrapper
 
 
 class MockGhost(GhostKernel):
     # 启动流程. 想用这种方式解耦掉系统文件读取等逻辑.
 
+    bootstrapper: ClassVar[List] = [
+        FileLoggerBootstrapper(),
+        RegisterThinkDemosBootstrapper(),
+        CommandFocusDriverBootstrapper(),
+        OpenAIBootstrapper(),
+
+        # 使用 llm chat completion 实现的思维
+        ConversationalThinksBootstrapper(),
+        # 使用 llm chat completion + function call 实现的思维.
+        FileAgentMindsetBootstrapper(),
+
+        # deprecated:
+        LLMConversationalThinkBootstrapper(),
+        LLMToolsFocusDriverBootstrapper(),
+        # 将 configs/llms/unitests 下的文件当成单元测试思维.
+        PromptUnitTestsBootstrapper(),
+        # 测试加入 undercover 游戏. deprecated
+        GameUndercoverBootstrapper(think_name="game/undercover"),
+    ]
+
+    depending_contracts: ClassVar[List] = [
+        LLMTextCompletion,
+        OpenAIChatCompletion,
+    ]
+
+    contracts_providers: ClassVar[List] = [
+        MockCacheProvider(),
+        MockAPIRepositoryProvider(),
+        MockMessageQueueProvider(),
+        MockOperationKernelProvider(),
+        MockThinkMetaDriverProvider(),
+    ]
+
     def get_bootstrapper(self) -> List[GhostBootstrapper]:
-        return [
-            FileLoggerBootstrapper(),
-            RegisterThinkDemosBootstrapper(),
-            CommandFocusDriverBootstrapper(),
-            OpenAIBootstrapper(),
-
-            # 使用 llm chat completion 实现的思维
-            ConversationalThinksBootstrapper(),
-            # 使用 llm chat completion + function call 实现的思维.
-            FileAgentMindsetBootstrapper(),
-
-            LLMConversationalThinkBootstrapper(),
-            LLMToolsFocusDriverBootstrapper(),
-            # sphero 的逻辑驱动.
-            SpheroGhostBootstrapper(),
-
-            # 将 configs/llms/unitests 下的文件当成单元测试思维.
-            PromptUnitTestsBootstrapper(),
-
-            # 测试加入 undercover 游戏. deprecated
-            GameUndercoverBootstrapper(think_name="game/undercover"),
-        ]
+        return self.bootstrapper
 
     def get_depending_contracts(self) -> List:
         contracts = super().get_depending_contracts()
-        appending = [LLMTextCompletion, OpenAIChatCompletion]
-        for i in appending:
-            contracts.append(i)
+        contracts += self.depending_contracts
         return contracts
 
     def get_contracts_providers(self) -> List[Provider]:
-        return [
-            MockCacheProvider(),
-            MockAPIRepositoryProvider(),
-            MockMessageQueueProvider(),
-            MockOperationKernelProvider(),
-            MockThinkMetaDriverProvider(),
-            # LocalThinkMetaStorageProvider(),
-        ]
+        return self.contracts_providers
---
+++ ghoshell/scripts/script_speech.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import argparse
+import os.path
+import sys
+from logging.config import dictConfig
+
+import yaml
+
+from ghoshell.container import Container
+from ghoshell.framework.ghost import GhostConfig
+from ghoshell.framework.shell import SyncGhostMessenger, MessageQueue
+from ghoshell.ghost import Ghost
+from ghoshell.mocks.ghost_mock import MockGhost
+from ghoshell.prototypes.playground.baidu_speech import BaiduSpeechShell
+from ghoshell.shell import Messenger
+
+
+def demo_ghost(root_path: str, root_container: Container) -> Ghost:
+    """
+    bootstrap demo ghost from local files in ./demo
+    """
+    container = root_container
+    config_path = "/".join([root_path, "configs", "ghost"])
+    runtime_path = "/".join([root_path, "runtime"])
+
+    config_file = config_path + "/config.yml"
+    with open(config_file, 'r', encoding='utf-8') as f:
+        config_data = yaml.safe_load(f)
+    config = GhostConfig(**config_data)
+
+    ghost = MockGhost(container, config, config_path, runtime_path)
+    return ghost
+
+
+def run_console_shell(root_path: str, root_container: Container):
+    """
+    run console shell with local demo ghost
+    """
+    container = root_container
+    config_path = "/".join([root_path, "configs", "shells/baidu_speech"])
+    runtime_path = "/".join([root_path, "runtime"])
+    shell = BaiduSpeechShell(container, config_path, runtime_path)
+    shell.bootstrap().run_as_app()
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="run ghoshell speech shell with local demo ghost and baidu speech")
+    parser.add_argument(
+        "--path", "-p",
+        nargs="?",
+        default="",
+        help="relative directory path that include config and runtime directories",
+        type=str,
+    )
+    parsed = parser.parse_args(sys.argv[1:])
+    relative = str(parsed.path)
+
+    cwd = os.getcwd()
+    root_path = cwd.rstrip("/") + "/" + relative.lstrip("/")
+    root_container = Container()
+
+    # register logger
+    with open(root_path + "/configs/logging.yaml", "r", encoding="utf-8") as f:
+        logging_config = yaml.safe_load(f)
+        dictConfig(logging_config)
+
+    ghost = demo_ghost(root_path, root_container)
+    ghost.boostrap()
+
+    message_queue = ghost.container.force_fetch(MessageQueue)
+    messenger = SyncGhostMessenger(ghost, queue=message_queue)
+    root_container.set(Messenger, messenger)
+    root_container.set(Ghost, ghost)
+    run_console_shell(root_path, root_container)
